3b8b3d1ae3ca735ca74ccdc6885d1567681898be
Andrew Dobrinskii <andrei.dobrinskii@electiconsulting.com>
2024-11-24 17:13:16 +0200

    add deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const repo = "WebMiner";
+var assetPrefix = `/${repo}/`;
+var basePath = `/${repo}`;
+const createNextIntlPlugin = require('next-intl/plugin');
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  }
+}
+
+//module.exports = withNextIntl(nextConfig);
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+//let assetPrefix = ''
+//let basePath = '/'
+
+if (isGithubActions) {
+
+  //   // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/`
+  basePath = ``
+  nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }
+}
+
+module.exports = withNextIntl(nextConfig);
+
+// module.exports = nextConfig
