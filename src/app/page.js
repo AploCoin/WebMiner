7450f7b644faa82ab6a05234905d939718042ad9
Andrew Dobrinskii <andrei.dobrinskii@electiconsulting.com>
2024-11-24 16:42:40 +0200

    initial

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+import WebMiner from '@/components/web-miner'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-12">
+      <WebMiner />
+    </main>
+  )
+}
